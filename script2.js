db5b213c583511249cbbaa5027de382391be6f4a
rickznX <rickelmefortes@gmail.com>
2024-10-20 23:13:35 -0300

    IFMA Campus Coelho Neto

---
+++ script2.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+});
